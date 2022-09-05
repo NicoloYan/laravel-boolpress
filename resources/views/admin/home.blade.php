f5ff03bf3bded5a7e56bf455af084dd6fa27db9b
NicoloYan <nico.yan@outlook.it>
2022-09-05 14:11:38 +0200

    Created backend scaffholding

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>Ciao sono la dash privata</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::middleware('auth')
+->name('admin.')
+->namespace('Admin')
+->prefix('admin')
+->group(function() {
+    Route::get('/', 'HomeController@index')->name('home');
+});
+
