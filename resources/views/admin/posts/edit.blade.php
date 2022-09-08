a8b83e6421a8db24bbe62c5de02c9e08675b7845
NicoloYan <nico.yan@outlook.it>
2022-09-08 15:29:39 +0200

    managed category select in edit

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -22,6 +22,19 @@
             <label for="title" class="form-label">Title</label>
             <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $post->title) }}">
         </div>
+
+        <div class="mb-3">
+            <label for="category_id">Category</label>
+            <select class="form-select" id="category_id" name="category_id">
+                <option value="">None</option>
+
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}"{{ old('category_id', $post->category_id) == $category->id ? 'selected' : '' }}>{{ $category->name }}</option>
+                @endforeach
+
+            </select>
+        </div>
+
         <div class="mb-3">
             <label for="content" class="form-label">Content</label>
             <textarea class="form-control" id="content" name="content" rows="6">{{ old('content', $post->content) }}</textarea>
